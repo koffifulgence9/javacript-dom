5b8bd0e6de6d7922dc323a38c92cd68b2f66855c
koffifulgence9 <49799547+koffifulgence9@users.noreply.github.com>
2025-04-22 19:22:05 +0000

    commit

--- initial/index.js
+++ initial/index.js
@@ -1,1 +1,9 @@
 // Code javascript
+import { data } from "./data.js";
+//afficher les produits
+const produitsData = document.querySelector(".produits");
+console.log(data);
+//
+
+const contenuHTML = `<div class="img">
+`;
